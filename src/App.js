395057e25f86194f2cef275e3757dbd9b72cbc08
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-09 12:29:56 -0600

    update-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React , {useEffect,useState} from 'react'
-import { BrowserRouter as Router , Routes , Route} from 'react-router-dom'
-import Amplify , {API , graphqlOperation} from 'aws-amplify';
-import awsExport from './aws-exports'
-import Home from './Home'
-Amplify.configure(awsExport)
+import {BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import './App.css';
+import Checkout from './pages/Checkout';
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Prendas from './pages/Prendas'
+import PrendaDetails from './pages/PrendaDetails';
+import Error from './pages/Error'
+import Admin from './pages/Admin';
+
 
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Routes >
-         <Route path='/' element={<Home/>}/>
+      <Router>
+      <Header/>
+        <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/prendas' element={<Prendas/>}/>
+       <Route path='/cart' element={<Cart/>}/>
+       <Route path='/checkout' element={<Checkout/>}/>
+       <Route path='/prendas/:id' element={<PrendaDetails/>}/>
+       <Route path='/error' element={<Error/>}/>
+       <Route path='/admin' element={<Admin/>}/>
        </Routes>
-     </Router>
+      </Router>
+    
     </div>
   );
 }
---
+++ src/context/jollas.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { v4 as uuidv4 } from "uuid";
+import { listVentas } from "../graphql/queries";
+import { processOrder } from "../graphql/mutations";
+
+
+const JollaContext = React.createContext();
+
+const JollaProvider = ({ children }) => {
+  const [jollas, setJollass] = useState([]);
+  const [featured, setFeatured] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchJollas();
+  }, []);
+
+  const checkout = async (orderDetails) => {
+    const payload = {
+      id: uuidv4(),
+      ...orderDetails
+    };
+    try {
+      await API.graphql(graphqlOperation(processOrder, { input: payload }));
+      console.log("Order is successful");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchJollas = async () => {
+    try {
+      setLoading(true);
+      // Switch authMode to API_KEY for public access
+      const { data } = await API.graphql({
+        query: listVentas,
+        authMode: "API_KEY"
+      });
+      const jollas = data.listVentas.items;
+      const featured = jollas.filter((jolla) => {
+        return !!jollas.featured;
+      });
+      setJollass(jollas);
+      setFeatured(featured);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <JollaContext.Provider value={{ jollas, featured, loading, checkout }}>
+      {children}
+    </JollaContext.Provider>
+  );
+};
+
+export { JollaContext, JollaProvider };
